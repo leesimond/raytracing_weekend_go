abb2d0f5e3d4263ae61e0e095392fe7ea883bcda
Simond Lee <simondlee@gmail.com>
2024-02-02 12:59:44 +1100

    feat: Output an image

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"raytracing_weekend_go/vector"
+	"os"
 )
 
+// "raytracing_weekend_go/vector"
+
+var imageWidth int = 256
+var imageHeight int = 256
+
 func main() {
-	fmt.Println("hello")
-	a := vector.Vec3{X: 1, Y: 2, Z: 3}
-	fmt.Println(a)
-	a.Negate()
-	fmt.Println(a)
-	a.Negate()
-	fmt.Println(a)
-	// b := a.Add(&vector.Vec3{X: 4, Y: 5, Z: 6})
-	a.Add(vector.Vec3{X: 4, Y: 5, Z: 6})
-	fmt.Println(a)
-	// fmt.Println(b)
-	a.Multiply(vector.Vec3{X: 7, Y: 8, Z: 9})
-	fmt.Println(a)
+	f := bufio.NewWriter(os.Stderr)
+	fmt.Printf("P3\n%d %d\n255\n", imageWidth, imageHeight)
+	for j := 0; j < imageHeight; j++ {
+		progress := fmt.Sprintf("\rScanlines remaining: %d ", imageHeight-j)
+		f.WriteString(progress)
+		f.Flush()
+		for i := 0; i < imageWidth; i++ {
+			r := float64(i) / float64(imageWidth-1)
+			g := float64(j) / float64(imageHeight-1)
+			b := 0
+
+			// var ir int = int(255.999 * r)
+			// var ig int = int(255.999 * g)
+			ir := int(255 * r)
+			ig := int(255 * g)
+
+			fmt.Printf("%d %d %d\n", ir, ig, b)
+		}
+	}
+	f.Write([]byte("\rDone.                 \n"))
+	f.Flush()
 }
