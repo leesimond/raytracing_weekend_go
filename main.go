ba5fe2feebf2b292552e44ae1365a418d3cf9bf1
Simond Lee <simondlee@gmail.com>
2024-02-09 11:15:04 +1100

    feat: Added ray to scene

--- main.go
+++ main.go
@@ -10,7 +10,13 @@ import (
 )
 
 func rayColour(r *ray.Ray) colour.Colour {
-	return colour.Colour{}
+	unitDirection := r.Direction.UnitVector()
+	a := 0.5 * (unitDirection.Y + 1)
+	colour1 := colour.Colour{X: 1, Y: 1, Z: 1}
+	colour1 = colour1.MultiplyScalar(1 - a)
+	colour2 := colour.Colour{X: 0.5, Y: 0.7, Z: 1}
+	colour2 = colour2.MultiplyScalar(a)
+	return colour1.Add(colour2)
 }
 
 func main() {
@@ -28,6 +34,21 @@ func main() {
 	viewportWidth := viewportHeight * (float64(imageWidth) / float64(imageHeight))
 	cameraCentre := vector.Point3{}
 
+	// Calculate the vectors across the horizontal and down the vertical viewport edges.
+	viewportU := vector.Vec3{X: viewportWidth}
+	viewportV := vector.Vec3{Y: -viewportHeight}
+
+	// Calculate the horizontal and vertical delta vectors from pixel to pixel.
+	pixelDeltaU := viewportU.DivideScalar(float64(imageWidth))
+	pixelDeltaV := viewportV.DivideScalar(float64(imageHeight))
+
+	// Calculate the location of the upper left pixel.
+	viewportUpperLeft := cameraCentre.Subtract(vector.Vec3{Z: focalLength})
+	viewportUpperLeft = viewportUpperLeft.Subtract(viewportU.DivideScalar(2))
+	viewportUpperLeft = viewportUpperLeft.Subtract(viewportV.DivideScalar(2))
+	pixelDelta := pixelDeltaU.Add(pixelDeltaV)
+	pixel00Loc := viewportUpperLeft.Add(pixelDelta.MultiplyScalar(0.5))
+
 	w := bufio.NewWriter(os.Stderr)
 	fmt.Printf("P3\n%d %d\n255\n", imageWidth, imageHeight)
 	for j := 0; j < imageHeight; j++ {
@@ -35,10 +56,12 @@ func main() {
 		w.WriteString(progress)
 		w.Flush()
 		for i := 0; i < imageWidth; i++ {
-			r := float64(i) / float64(imageWidth-1)
-			g := float64(j) / float64(imageHeight-1)
-			b := 0.0
-			pixelColour := colour.Colour{X: r, Y: g, Z: b}
+			pixelDeltaUI := pixelDeltaU.MultiplyScalar(float64(i))
+			pixelDeltaVJ := pixelDeltaV.MultiplyScalar(float64(j))
+			pixelCentre := pixel00Loc.Add(pixelDeltaUI.Add(pixelDeltaVJ))
+			rayDirection := pixelCentre.Subtract(cameraCentre)
+			r := ray.New(cameraCentre, rayDirection)
+			pixelColour := rayColour(&r)
 			colour.WriteColour(os.Stdout, pixelColour)
 		}
 	}
--- ray/ray.go
+++ ray/ray.go
@@ -4,9 +4,13 @@ import "raytracing_weekend_go/vector"
 
 type Ray struct {
 	origin    vector.Point3
-	direction vector.Vec3
+	Direction vector.Vec3
+}
+
+func New(origin vector.Point3, direction vector.Vec3) Ray {
+	return Ray{origin: origin, Direction: direction}
 }
 
 func (r *Ray) At(t float64) vector.Point3 {
-	return r.origin.Add(r.direction.MultiplyScalar(t))
+	return r.origin.Add(r.Direction.MultiplyScalar(t))
 }
