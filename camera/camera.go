553527484bbdcaa07fd7c02fda0e2bd5b0add8ca
Simond Lee <simondlee@gmail.com>
2024-03-15 13:44:09 +1100

    fix: true lambertian reflection

--- camera/camera.go
+++ camera/camera.go
@@ -110,7 +110,7 @@ func rayColour(r *ray.Ray, depth int, world hittable.Hittable) colour.Colour {
 	}
 
 	if world.Hit(r, &interval.Interval{Min: 0.001, Max: math.Inf(1)}, &rec) {
-		direction := vector.RandomOnHemisphere(&rec.Normal)
+		direction := rec.Normal.Add(vector.RandomUnitVector())
 		rayColour := rayColour(&ray.Ray{Origin: rec.P, Direction: direction}, depth-1, world)
 		return rayColour.MultiplyScalar(0.5)
 	}
